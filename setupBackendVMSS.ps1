a9711cef44105941a3c9805430591eca418b2e91
Muneer Syed <sdmuneer89@gmail.com>
2021-03-12 15:38:48 +0530

    Add files via upload

---
+++ setupBackendVMSS.ps1
@@ -0,0 +1,29 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); 
+
+choco upgrade git -yr --no-progress
+choco upgrade directx -yr --no-progress
+
+New-Alias -Name git -Value "$Env:ProgramFiles\Git\bin\git.exe" -Force
+
+$folder = "c:\Unreal\"
+if (-not (Test-Path -LiteralPath $folder)) {
+    git clone -q https://github.com/jmarymee/Unreal-Pixel-Streaming-on-Azure.git $folder
+}
+else {
+    #rename the existing folder if exists
+    $endtag = 'unreal-' + (get-date).ToString('MMddyyhhmmss')
+    Rename-Item -Path $folder  -NewName $endtag -Force
+    git clone -q https://github.com/jmarymee/Unreal-Pixel-Streaming-on-Azure.git $folder
+}
+
+Invoke-WebRequest https://unrealbackendfiles.blob.core.windows.net/ourpublicblobs/WindowsNoEditor.zip -OutFile C:\WindowsNoEditor.zip
+
+$blobDestination = $folder + '\iac\unreal\app'
+$zipFileName = 'C:\WindowsNoEditor.zip'
+
+Expand-Archive -LiteralPath $zipFileName -DestinationPath $blobDestination
+
+exit 0
---
+++ setupMatchMakerVM.ps1
@@ -0,0 +1,29 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); 
+
+choco upgrade filezilla -yr --no-progress
+choco upgrade git -yr --no-progress
+choco upgrade nodejs -yr --no-progress
+choco upgrade vcredist-all -yr --no-progress
+choco upgrade directx -yr --no-progress
+
+New-Alias -Name git -Value "$Env:ProgramFiles\Git\bin\git.exe" -Force
+
+#export GITHUB_USER=anonuser
+#export GITHUB_TOKEN=(az keyvault secret show -n thekey --vault-name uegamingakv | ConvertFrom-Json).value
+export GITHUB_REPOSITORY=jmarymee/Unreal-Pixel-Streaming-on-Azure
+
+$folder = "c:\Unreal\"
+if (-not (Test-Path -LiteralPath $folder)) {
+    #git clone -q https://github.com/Azure/Unreal-Pixel-Streaming-on-Azure.git $folder
+    git clone https://github.com/${GITHUB_REPOSITORY} $folder
+}
+else {
+    #rename the existing folder
+    $endtag = 'unreal-' + (get-date).ToString('MMddyyhhmmss')
+    Rename-Item -Path $folder  -NewName $endtag -Force
+    #git clone -q https://github.com/Azure/Unreal-Pixel-Streaming-on-Azure.git $folder
+    git clone https://github.com/${GITHUB_REPOSITORY} $folder
+}
+exit 0
