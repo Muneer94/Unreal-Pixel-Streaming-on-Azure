a05e8d4bfd0e1cc4621b4f7b4ce1807e0e1c4acd
Muneer Syed <sdmuneer89@gmail.com>
2021-03-31 17:55:51 +0530

    Update setupBackendVMSS.ps1

--- setupBackendVMSS.ps1
+++ setupBackendVMSS.ps1
@@ -3,27 +3,39 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); 
 
-choco upgrade git -yr --no-progress
 choco upgrade directx -yr --no-progress
 
-New-Alias -Name git -Value "$Env:ProgramFiles\Git\bin\git.exe" -Force
-
 $folder = "c:\Unreal\"
+
 if (-not (Test-Path -LiteralPath $folder)) {
-    git clone -q https://github.com/jmarymee/Unreal-Pixel-Streaming-on-Azure.git $folder
+    New-Item -ItemType Directory -Force -Path $folder
 }
 else {
     #rename the existing folder if exists
     $endtag = 'unreal-' + (get-date).ToString('MMddyyhhmmss')
     Rename-Item -Path $folder  -NewName $endtag -Force
-    git clone -q https://github.com/jmarymee/Unreal-Pixel-Streaming-on-Azure.git $folder
+    New-Item -ItemType Directory -Force -Path $folder
 }
 
-Invoke-WebRequest https://unrealbackendfiles.blob.core.windows.net/ourpublicblobs/WindowsNoEditor.zip -OutFile C:\WindowsNoEditor.zip
+$connectTestResult = Test-NetConnection -ComputerName d88bcstoracct1.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # Save the password so the drive will persist on reboot
+    cmd.exe /C "cmdkey /add:`"d88bcstoracct1.file.core.windows.net`" /user:`"Azure\d88bcstoracct1`" /pass:`"0UMWfVn7HpP7FeDq7njUtg/EELN4P53+hD3ew0AhxCEvVXgIDQtgovSBRMLnU/9+4W+cbguAfY0stkqat3II/g==`""
+    # Mount the drive
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\d88bcstoracct1.file.core.windows.net\ntarunrealfs" -Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
 
-$blobDestination = $folder + '\iac\unreal\app'
-$zipFileName = 'C:\WindowsNoEditor.zip'
+$zipFileName = 'Nextech UE4 PS Files - 3-9-21.zip'
+$remoteStoragePath = 'Z:\' + $zipFileName
+$zipFilePath = $folder + $zipFileName
 
-Expand-Archive -LiteralPath $zipFileName -DestinationPath $blobDestination
+Copy-Item $remoteStoragePath -Destination $folder
+Expand-Archive -LiteralPath $zipFilePath -DestinationPath $folder
 
 exit 0
+
+Remove-Item C:\ProgramData\chocolatey -Recurse
+
+Start-Process -NoNewWindow 'c:\Unreal\Nextech UE4 PS Files - 3-9-21\WindowsNoEditor\NexTechAR.exe' -AudioMixer -PixelStreamingIP=localhost -PixelStreamingPort=8888 -RenderOffScreen
