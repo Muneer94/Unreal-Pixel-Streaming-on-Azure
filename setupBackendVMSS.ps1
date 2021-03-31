5c3b4492a03a0581391ccbccbc851585f7ac7e65
Muneer Syed <sdmuneer89@gmail.com>
2021-03-31 18:58:47 +0530

    Update setupBackendVMSS.ps1

--- setupBackendVMSS.ps1
+++ setupBackendVMSS.ps1
@@ -17,16 +17,6 @@ else {
     New-Item -ItemType Directory -Force -Path $folder
 }
 
-$connectTestResult = Test-NetConnection -ComputerName d88bcstoracct1.file.core.windows.net -Port 445
-if ($connectTestResult.TcpTestSucceeded) {
-    # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"d88bcstoracct1.file.core.windows.net`" /user:`"Azure\d88bcstoracct1`" /pass:`"0UMWfVn7HpP7FeDq7njUtg/EELN4P53+hD3ew0AhxCEvVXgIDQtgovSBRMLnU/9+4W+cbguAfY0stkqat3II/g==`""
-    # Mount the drive
-    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\d88bcstoracct1.file.core.windows.net\ntarunrealfs" -Persist
-} else {
-    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
-}
-
 $zipFileName = 'Nextech UE4 PS Files - 3-9-21.zip'
 $remoteStoragePath = 'Z:\' + $zipFileName
 $zipFilePath = $folder + $zipFileName
